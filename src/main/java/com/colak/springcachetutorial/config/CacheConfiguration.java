f78779a41b81b0325ee38b52c93cdc9acbd0bde7
Orçun Çolak <orcun.colak@hazelcast.com>
2024-07-24 09:34:18 +0300

    upgrade spring

--- src/main/java/com/colak/springcachetutorial/config/CacheConfiguration.java
+++ src/main/java/com/colak/springcachetutorial/config/CacheConfiguration.java
@@ -2,8 +2,6 @@ package com.colak.springcachetutorial.config;
 
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.config.XmlClientConfigBuilder;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +10,6 @@ import java.io.IOException;
 
 @Configuration
 @EnableCaching
-
-@RequiredArgsConstructor
-@Slf4j
 public class CacheConfiguration {
     @Bean
     public ClientConfig clientConfig() throws IOException {
